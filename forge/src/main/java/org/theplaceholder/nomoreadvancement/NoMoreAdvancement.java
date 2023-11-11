c3415f0e9a2e38ac62364d2f497706d8704260f7
theplaceholder <guekbaile@gmail.com>
2023-11-11 15:24:02 +0100

    Initial commit

---
+++ forge/src/main/java/org/theplaceholder/nomoreadvancement/NoMoreAdvancement.java
@@ -0,0 +1,8 @@
+package org.theplaceholder.nomoreadvancement;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("nomoreadvancement")
+public class NoMoreAdvancement {
+    public NoMoreAdvancement() {}
+}

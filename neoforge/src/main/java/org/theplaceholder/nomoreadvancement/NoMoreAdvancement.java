d0f862b91b1b3ac4eb2dea14d5e5ca0972849db2
theplaceholder <handy.baile@gmail.com>
2024-07-04 09:10:41 +0200

    1.21

--- neoforge/src/main/java/org/theplaceholder/nomoreadvancement/NoMoreAdvancement.java
+++ neoforge/src/main/java/org/theplaceholder/nomoreadvancement/NoMoreAdvancement.java
@@ -1,6 +1,6 @@
 package org.theplaceholder.nomoreadvancement;
 
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod("nomoreadvancement")
 public class NoMoreAdvancement {
